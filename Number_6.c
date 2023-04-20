7260d24624c977d06a341f6dbcb0196ed46ee035
chingachguk <kirillkuceruk063@gmail.com>
2023-04-20 18:56:42 +1000

    Открытие

--- Number_6.c
+++ Number_6.c
@@ -1,2 +1,9 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+
+int main() {
+    FILE* fp = fopen("input.txt", "r"); // открыть файл на чтение
+    if (fp == NULL) { // обработка ошибок при открытии файла
+        printf("Error: failed to open the file.\n");
+        return 1;
+    }
